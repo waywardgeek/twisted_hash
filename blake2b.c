81f577af4371088150cb78bfdda0a10e8ecb427b
Bill Cox <waywardgeek@google.com>
2016-02-10 09:46:11 -0800

    Removed updates to 'h' registers between 2-round hashing steps, since this considerably slowed down hashing.  If the security was good before, this change likely breaks it, but it does show the speed possible with this idea.

--- blake2b.c
+++ blake2b.c
@@ -246,7 +246,7 @@ int blake2b_init_key( blake2b_state *S, const uint8_t outlen, const void *key, c
   return 0;
 }
 
-static inline int blake2b_compress( blake2b_state *S, const uint8_t block[BLAKE2B_BLOCKBYTES], uint8_t round )
+static inline int blake2b_twisted_compress( blake2b_state *S, const uint8_t block[BLAKE2B_BLOCKBYTES] )
 {
   __m128i row1l, row1h;
   __m128i row2l, row2h;
@@ -254,37 +254,18 @@ static inline int blake2b_compress( blake2b_state *S, const uint8_t block[BLAKE2
   __m128i row4l, row4h;
   __m128i b0, b1;
   __m128i t0, t1;
-#if defined(HAVE_SSSE3) && !defined(HAVE_XOP)
   const __m128i r16 = _mm_setr_epi8( 2, 3, 4, 5, 6, 7, 0, 1, 10, 11, 12, 13, 14, 15, 8, 9 );
   const __m128i r24 = _mm_setr_epi8( 3, 4, 5, 6, 7, 0, 1, 2, 11, 12, 13, 14, 15, 8, 9, 10 );
-#endif
-#if defined(HAVE_SSE41)
-  const __m128i m0 = LOADU( block + 00 );
-  const __m128i m1 = LOADU( block + 16 );
-  const __m128i m2 = LOADU( block + 32 );
-  const __m128i m3 = LOADU( block + 48 );
-  const __m128i m4 = LOADU( block + 64 );
-  const __m128i m5 = LOADU( block + 80 );
-  const __m128i m6 = LOADU( block + 96 );
-  const __m128i m7 = LOADU( block + 112 );
-#else
-  const uint64_t  m0 = ( ( uint64_t * )block )[ 0];
-  const uint64_t  m1 = ( ( uint64_t * )block )[ 1];
-  const uint64_t  m2 = ( ( uint64_t * )block )[ 2];
-  const uint64_t  m3 = ( ( uint64_t * )block )[ 3];
-  const uint64_t  m4 = ( ( uint64_t * )block )[ 4];
-  const uint64_t  m5 = ( ( uint64_t * )block )[ 5];
-  const uint64_t  m6 = ( ( uint64_t * )block )[ 6];
-  const uint64_t  m7 = ( ( uint64_t * )block )[ 7];
-  const uint64_t  m8 = ( ( uint64_t * )block )[ 8];
-  const uint64_t  m9 = ( ( uint64_t * )block )[ 9];
-  const uint64_t m10 = ( ( uint64_t * )block )[10];
-  const uint64_t m11 = ( ( uint64_t * )block )[11];
-  const uint64_t m12 = ( ( uint64_t * )block )[12];
-  const uint64_t m13 = ( ( uint64_t * )block )[13];
-  const uint64_t m14 = ( ( uint64_t * )block )[14];
-  const uint64_t m15 = ( ( uint64_t * )block )[15];
-#endif
+  __m128i m0, m1, m2, m3, m4, m5, m6, m7;
+#define LOADBLOCK(i) \
+  m0 = LOADU( block + (i * 128 + 00) ); \
+  m1 = LOADU( block + (i * 128 + 16) ); \
+  m2 = LOADU( block + (i * 128 + 32) ); \
+  m3 = LOADU( block + (i * 128 + 48) ); \
+  m4 = LOADU( block + (i * 128 + 64) ); \
+  m5 = LOADU( block + (i * 128 + 80) ); \
+  m6 = LOADU( block + (i * 128 + 96) ); \
+  m7 = LOADU( block + (i * 128 + 112) );
   row1l = LOADU( &S->h[0] );
   row1h = LOADU( &S->h[2] );
   row2l = LOADU( &S->h[4] );
@@ -293,32 +274,71 @@ static inline int blake2b_compress( blake2b_state *S, const uint8_t block[BLAKE2
   row3h = LOADU( &blake2b_IV[2] );
   row4l = _mm_xor_si128( LOADU( &blake2b_IV[4] ), LOADU( &S->t[0] ) );
   row4h = _mm_xor_si128( LOADU( &blake2b_IV[6] ), LOADU( &S->f[0] ) );
-  switch( round ) {
-  case 0:
-      ROUND( 0 );
-      ROUND( 1 );
-      break;
-  case 2:
-      ROUND( 2 );
-      ROUND( 3 );
-      break;
-  case 4:
-      ROUND( 4 );
-      ROUND( 5 );
-      break;
-  case 6:
-      ROUND( 6 );
-      ROUND( 7 );
-      break;
-  case 8:
-      ROUND( 8 );
-      ROUND( 9 );
-      break;
-  case 10:
-      ROUND( 10 );
-      ROUND( 11 );
-      break;
-  }
+
+  LOADBLOCK(0);
+  ROUND( 0 );
+  ROUND( 1 );
+
+  LOADBLOCK(1);
+  ROUND( 2 );
+  ROUND( 3 );
+
+  LOADBLOCK(2);
+  ROUND( 4 );
+  ROUND( 5 );
+
+  LOADBLOCK(3);
+  ROUND( 6 );
+  ROUND( 7 );
+
+  LOADBLOCK(4);
+  ROUND( 8 );
+  ROUND( 9 );
+
+  LOADBLOCK(5);
+  ROUND( 10 );
+  ROUND( 11 );
+
+  LOADBLOCK(6);
+  ROUND( 0 );
+  ROUND( 1 );
+
+  LOADBLOCK(7);
+  ROUND( 2 );
+  ROUND( 3 );
+
+  LOADBLOCK(4);
+  ROUND( 4 );
+  ROUND( 5 );
+
+  LOADBLOCK(1);
+  ROUND( 6 );
+  ROUND( 7 );
+
+  LOADBLOCK(6);
+  ROUND( 8 );
+  ROUND( 9 );
+
+  LOADBLOCK(3);
+  ROUND( 10 );
+  ROUND( 11 );
+
+  LOADBLOCK(0);
+  ROUND( 0 );
+  ROUND( 1 );
+
+  LOADBLOCK(5);
+  ROUND( 2 );
+  ROUND( 3 );
+
+  LOADBLOCK(2);
+  ROUND( 4 );
+  ROUND( 5 );
+
+  LOADBLOCK(7);
+  ROUND( 6 );
+  ROUND( 7 );
+
   row1l = _mm_xor_si128( row3l, row1l );
   row1h = _mm_xor_si128( row3h, row1h );
   STOREU( &S->h[0], _mm_xor_si128( LOADU( &S->h[0] ), row1l ) );
@@ -328,26 +348,6 @@ static inline int blake2b_compress( blake2b_state *S, const uint8_t block[BLAKE2
   STOREU( &S->h[4], _mm_xor_si128( LOADU( &S->h[4] ), row2l ) );
   STOREU( &S->h[6], _mm_xor_si128( LOADU( &S->h[6] ), row2h ) );
   return 0;
-}
-
-static inline int blake2b_twisted_compress( blake2b_state *S, const uint8_t block[8*BLAKE2B_BLOCKBYTES] ) {
-    blake2b_compress( S, block + 0*BLAKE2B_BLOCKBYTES, 0 );
-    blake2b_compress( S, block + 1*BLAKE2B_BLOCKBYTES, 2 );
-    blake2b_compress( S, block + 2*BLAKE2B_BLOCKBYTES, 4 );
-    blake2b_compress( S, block + 3*BLAKE2B_BLOCKBYTES, 6 );
-    blake2b_compress( S, block + 4*BLAKE2B_BLOCKBYTES, 8 );
-    blake2b_compress( S, block + 5*BLAKE2B_BLOCKBYTES, 10 );
-    blake2b_compress( S, block + 6*BLAKE2B_BLOCKBYTES, 0 );
-    blake2b_compress( S, block + 7*BLAKE2B_BLOCKBYTES, 2 );
-    blake2b_compress( S, block + 4*BLAKE2B_BLOCKBYTES, 4 );
-    blake2b_compress( S, block + 1*BLAKE2B_BLOCKBYTES, 6 );
-    blake2b_compress( S, block + 6*BLAKE2B_BLOCKBYTES, 8 );
-    blake2b_compress( S, block + 3*BLAKE2B_BLOCKBYTES, 10 );
-    blake2b_compress( S, block + 0*BLAKE2B_BLOCKBYTES, 0 );
-    blake2b_compress( S, block + 5*BLAKE2B_BLOCKBYTES, 2 );
-    blake2b_compress( S, block + 2*BLAKE2B_BLOCKBYTES, 4 );
-    blake2b_compress( S, block + 7*BLAKE2B_BLOCKBYTES, 6 );
-    return 0;
 }
 
 int blake2b_update( blake2b_state *S, const uint8_t *in, uint64_t inlen )
@@ -396,7 +396,7 @@ int blake2b_final( blake2b_state *S, uint8_t *out, uint8_t outlen )
 
   blake2b_increment_counter( S, S->buflen );
   blake2b_set_lastblock( S );
-  memset( S->buf + S->buflen, 0, 16 * BLAKE2B_BLOCKBYTES - S->buflen ); /* Padding */
+  memset( S->buf + S->buflen, 0, 8 * BLAKE2B_BLOCKBYTES - S->buflen ); /* Padding */
   blake2b_twisted_compress( S, S->buf );
   memcpy( out, &S->h[0], outlen );
   return 0;
